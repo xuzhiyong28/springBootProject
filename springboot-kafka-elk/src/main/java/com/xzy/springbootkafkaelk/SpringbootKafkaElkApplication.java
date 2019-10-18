d4e1ac82257c2b7aaae327fb57c1d43946635a84
xuzhiyong28 <jip0574@qq.com>
2019-10-18 15:08:02 +0800

    update springbootproject

---
+++ springboot-kafka-elk/src/main/java/com/xzy/springbootkafkaelk/SpringbootKafkaElkApplication.java
@@ -0,0 +1,13 @@
+package com.xzy.springbootkafkaelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaElkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaElkApplication.class, args);
+	}
+
+}
---
+++ springboot-kafka-elk/src/test/java/com/xzy/springbootkafkaelk/SpringbootKafkaElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzy.springbootkafkaelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
