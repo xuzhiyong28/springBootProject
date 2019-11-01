fe0db470a7aefa6d001b457b095c6303ddf8c345
xuzhiyong28 <jip0574@qq.com>
2019-11-01 16:26:18 +0800

    新建disrutor新建disrutor

---
+++ springboot-disrutor-demo/src/main/java/com/xzy/springbootdisrutordemo/SpringbootDisrutorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xzy.springbootdisrutordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDisrutorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDisrutorDemoApplication.class, args);
+	}
+
+}
