c58f08749c9fcfcd0cf0b7f640cc1ced469a61c7
xuzhiyong28 <jip0574@qq.com>
2020-01-20 16:32:18 +0800

    修改代码修改代码修改代码

---
+++ springboot-datasource-common/src/main/java/com/xzy/datasourcecommon/SpringbootDatasourceCommonApplication.java
@@ -0,0 +1,13 @@
+package com.xzy.datasourcecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatasourceCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDatasourceCommonApplication.class, args);
+	}
+
+}
---
+++ springboot-datasource-common/src/test/java/com/xzy/datasourcecommon/SpringbootDatasourceCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzy.datasourcecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatasourceCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
